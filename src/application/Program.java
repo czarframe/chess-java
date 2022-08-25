a1933651459434f4df9231e325a1c21f1988b36a
czarframe <cesaragstincs@gmail.com>
2022-08-25 14:30:26 -0300

    Primeira classe: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,12 @@
-
 package application;
 
+import boardgame.Position;
 
 public class Program {
 
     public static void main(String[] args) {
-     
+        Position position = new Position(1,4);
+        System.out.println(position);
     }
-
+    
 }
