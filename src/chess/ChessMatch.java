f5b81f8d54992553e99a9cfb6731f3da07da7940
czarframe <cesaragstincs@gmail.com>
2022-10-08 14:34:55 -0300

    Tratando exceções e limpando a tela

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -41,7 +41,7 @@ public class ChessMatch {
     
     private void validateSourcePosition(Position position){
         if(!board.thereIsAPiece(position)){
-            throw new ChessException("Não existe peça na position de origem!");
+            throw new ChessException("Não existe peça na posição de origem!");
         }
     }
     
