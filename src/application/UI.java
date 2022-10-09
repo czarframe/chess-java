99904ce97b5b4780fc8a10a34ecb50f553393771
czarframe <cesaragstincs@gmail.com>
2022-10-09 14:01:44 -0300

    Trocando de jogador a cada turno

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,5 +1,6 @@
 package application;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -43,6 +44,13 @@ public class UI {
             catch (RuntimeException e){
                 throw new InputMismatchException("Erro na leitura da posição! Valores válidos são de a1 à h8");
             }
+        }
+        
+        public static void printMatch(ChessMatch chessMatch){
+            printBoard(chessMatch.getPieces());
+            System.out.println("");
+            System.out.println("Turno: " + chessMatch.getTurn());
+            System.out.println("Esperando jogador: " + chessMatch.getCurrentPlayer());
         }
         
 	public static void printBoard(ChessPiece[][] pieces) {
